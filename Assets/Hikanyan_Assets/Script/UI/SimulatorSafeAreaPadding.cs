d25c8a4bb8946c9d95c44df6ed280eb27bf6e72e
Hikanyan <hikanyan0131@gmail.com>
2023-05-28 17:25:36 +0900

    Windows環境とSimulator(スマホ)環境で異なるRectTransformを持つゲームオブジェクトを切り替えることができる

---
+++ Assets/Hikanyan_Assets/Script/UI/SimulatorSafeAreaPadding.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+[ExecuteAlways]
+public class SimulatorSafeAreaPadding : MonoBehaviour
+{
+
+    private DeviceOrientation _postOrientation;
+    void Update()
+    {
+        if (Input.deviceOrientation != DeviceOrientation.Unknown && _postOrientation == Input.deviceOrientation)
+            return;
+
+        _postOrientation = Input.deviceOrientation;
+
+        var rect = GetComponent<RectTransform>();
+        var area = Screen.safeArea;
+        var resolition = Screen.currentResolution;
+
+        rect.sizeDelta = Vector2.zero;
+        rect.anchorMax = new Vector2(area.xMax / resolition.width, area.yMax / resolition.height);
+        rect.anchorMin = new Vector2(area.xMin / resolition.width, area.yMin / resolition.height);
+    }
+}

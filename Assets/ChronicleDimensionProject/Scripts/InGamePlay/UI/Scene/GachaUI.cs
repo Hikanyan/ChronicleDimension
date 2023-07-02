744cf00df009386f2d8557884db6eaa8ff7f7ff2
Hikanyan <hikanyan0131@gmail.com>
2023-07-02 22:04:44 +0900

    [Add]UI

---
+++ Assets/ChronicleDimensionProject/Scripts/InGamePlay/UI/Scene/GachaUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+public class GachaUI : UIBase
+{
+    // Gacha UIの実装
+    
+    [SerializeField] Image resultImage;
+
+    private void Start()
+    {
+        // 初期状態で非表示にする
+        resultImage.gameObject.SetActive(false);
+    }
+
+    public void  ShowResult(GachaItem item)
+    {
+        // ガチャの結果を非同期に処理
+
+        // 結果の表示
+
+        // 結果のアニメーション
+    }
+}

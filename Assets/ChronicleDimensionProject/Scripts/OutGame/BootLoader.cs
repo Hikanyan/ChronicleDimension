c271a565ab1eedf5e0b3908048ae6af2ae9b9e11
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-05 23:41:39 +0900

    [Fix]Script修正

--- Assets/ChronicleDimensionProject/Scripts/OutGame/BootLoader.cs
+++ Assets/ChronicleDimensionProject/Scripts/OutGame/BootLoader.cs
@@ -3,20 +3,17 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-namespace Hikanyan.Gameplay
-{
-    /// <summary>
-    /// 開始時に SequenceManager をインスタンス化して初期化します。
-    /// </summary>
-    public class BootLoader : MonoBehaviour
-    {
-        [SerializeField]
-        SequenceManager sequenceManagerPrefab;
 
-        void Start()
-        {
-            Instantiate(sequenceManagerPrefab);
-            SequenceManager.Instance.Initialize();
-        }
+/// <summary>
+/// 開始時に SequenceManager をインスタンス化して初期化します。
+/// </summary>
+public class BootLoader : MonoBehaviour
+{
+    [SerializeField] SequenceManager sequenceManagerPrefab;
+
+    void Start()
+    {
+        Instantiate(sequenceManagerPrefab);
+        SequenceManager.Instance.Initialize();
     }
 }

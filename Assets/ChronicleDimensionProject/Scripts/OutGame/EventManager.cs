9c2426c4c2f2a94dc80f0fa01f5efc63346051aa
Hikanyan <hikanyan0131@gmail.com>
2023-07-12 23:41:32 +0900

    [Add]EventManager

---
+++ Assets/ChronicleDimensionProject/Scripts/OutGame/EventManager.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//イベントを管理するクラス
+public class EventManager : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}

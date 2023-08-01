fa5fded28625cb77fe46f9429a0a425da4226919
Hikanyan <hikanyan0131@gmail.com>
2023-08-01 21:19:35 +0900

    [Delete]

--- Assets/ChronicleDimensionProject/Scripts/Core/UI/Scene/ExplanationUI.cs
+++ Assets/ChronicleDimensionProject/Scripts/Core/UI/Scene/ExplanationUI.cs
@@ -2,14 +2,6 @@
 {
     // Explanation UIの実装を書いてください
     // このクラスはUIBaseを継承しています
-    // このクラスはシングルトンです
-    // このクラスはGameManagerのExplanationStateによって表示されます
-    // このクラスはGameManagerのExplanationStateがExplanation以外になったら非表示になります
-    // このクラスはGameManagerのExplanationStateがExplanation以外になったらExplanationUIのインスタンスを破棄します
-    // このクラスはGameManagerのExplanationStateがExplanation以外になったらGameManagerのExplanationStateをNoneにします
-    // このクラスはGameManagerのExplanationStateがExplanation以外になったらGameManagerのGameStateをTitleにします
-    // このクラスはGameManagerのExplanationStateがExplanation以外になったらGameManagerのExplanationStateをNoneにします
-    // このクラスはGameManagerのExplanationStateがExplanation以外になったらGameManagerのGameStateをTitleにします 
-    // このクラスはGameManagerのExplanationStateがExplanation以外になったらGameManagerのExplanationStateをNoneにします
+    
     
 }

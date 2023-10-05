5451dcbcb5fcec2f802023932a47a0874f6c50a8
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-10-05 15:47:02 +0900

    [Update]Action

--- Assets/ChronicleDimensionProject/Scripts/Core/UI/Scene/Title.cs
+++ Assets/ChronicleDimensionProject/Scripts/Core/UI/Scene/Title.cs
@@ -7,17 +7,22 @@ public class Title : UIBase
     [SerializeField] private GameObject titleUI;
     [SerializeField] private CRIAudioList bgmName;
 
+    private Action _start;
 
     // Titleの実装
-    void OnEnable()
+    void TitleStart()
     {
         //ログイン、BGM、TitleUIの表示
         CRIAudioManager.Instance.CribgmPlay(bgmName);
-    }
-    
-    private void Update()
-    {
-        
+
+        // GameManagerのOnGameStartイベントにイベントハンドラを登録
+        GameManager.Instance.OnGameStart += HandleGameStart;
     }
 
+    void HandleGameStart()
+    {
+        // ゲームが開始されたときの処理をここに記述
+        // 例えば、タイトルUIの非表示化など
+        titleUI.SetActive(false);
+    }
 }

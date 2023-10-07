5b13335089aeae82ec5cfbb08eac91fb55603bce
Hikanyan <hikanyan0131@gmail.com>
2023-10-07 13:23:43 +0900

    [Update]Notes

---
+++ Assets/ChronicleDimensionProject/Scripts/InGamePlay/RhythmGame/System/Notes/Notes.cs
@@ -0,0 +1,49 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public enum NotesType
+{
+    None,
+    TapNote,
+    SkillNote,
+    FlickNote,
+    DamageNote,
+    LongNote,
+}
+
+public class Notes : MonoBehaviour
+{
+    float _time = 0.00f;
+    private float _endTime = 0.00f; // ホールド
+    int _block = 0;
+    NotesType _type;
+    bool _visible = false;
+    bool _disable = false;
+    bool _holding = false; // ホールド
+
+    public void SetVisible(bool visible)
+    {
+        if (visible)
+        {
+            gameObject.SetActive(true);
+            _visible = true;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            _visible = false;
+            transform.position = new Vector3(0, 0, -10);
+        }
+    }
+
+    public void SetNotesPos(float posz)
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, posz);
+    }
+
+    public void SetEndNotesPos(float posz)
+    {
+        GameObject endObject = Instantiate(gameObject);
+        endObject.transform.position = new Vector3(transform.position.x, transform.position.y, posz);
+    }
+}

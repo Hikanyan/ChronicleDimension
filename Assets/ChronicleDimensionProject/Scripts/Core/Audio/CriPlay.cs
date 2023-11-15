72caba367bd32204961369ea0bd563f2f5505096
Hikanyan <hikanyan0131@gmail.com>
2023-11-16 02:43:16 +0900

    [Update]CRIが動く

--- Assets/ChronicleDimensionProject/Scripts/Core/Audio/CriPlay.cs
+++ Assets/ChronicleDimensionProject/Scripts/Core/Audio/CriPlay.cs
@@ -8,7 +8,7 @@ public class CriPlay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CriAudioManager.Instance.PlayBGM(CriAudioManager.CueSheet.Bgm,"");
+        CriAudioManager.Instance.PlayBGM(CriAudioManager.CueSheet.Bgm,"BGM_Running Through The Galaxy_BPM150 20220630");
     }
 
     // Update is called once per frame

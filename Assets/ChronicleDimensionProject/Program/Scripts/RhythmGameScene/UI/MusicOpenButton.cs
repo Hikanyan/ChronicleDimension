b62b4ef114b1e707ece6e46351cb3848c5733f40
Hikanyan <hikanyan0131@gmail.com>
2024-02-19 19:12:45 +0900

    [Add,Update] A.I.VOICE for GAMESをインストール UIの修正

---
+++ Assets/ChronicleDimensionProject/Program/Scripts/RhythmGameScene/UI/MusicOpenButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ChronicleDimensionProject.UI
+{
+    public class MusicOpenButton
+    {
+        public void SetMusicData(GameObject musicData)
+        {
+            
+        }
+    }
+}

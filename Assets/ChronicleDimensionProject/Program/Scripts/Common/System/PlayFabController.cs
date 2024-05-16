423648320b6be949249d4fdbf4323a7855c0efd5
Hikanyan <hikanyan0131@gmail.com>
2024-05-16 11:25:53 +0900

    [Update]

--- Assets/ChronicleDimensionProject/Program/Scripts/Common/System/PlayFabController.cs
+++ Assets/ChronicleDimensionProject/Program/Scripts/Common/System/PlayFabController.cs
@@ -2,17 +2,20 @@ using PlayFab;
 using PlayFab.ClientModels;
 using UnityEngine;
 
-public class PlayFabController : MonoBehaviour
+namespace ChronicleDimensionProject
 {
-    void Start()
+    public class PlayFabController : MonoBehaviour
     {
-        PlayFabClientAPI.LoginWithCustomID(new LoginWithCustomIDRequest
-            {
-                CustomId = SystemInfo.deviceUniqueIdentifier,
-                CreateAccount = true
-            }
-            ,
-            result => { Debug.Log("ログイン成功"); },
-            error => { Debug.Log("ログイン失敗"); });
+        void Start()
+        {
+            PlayFabClientAPI.LoginWithCustomID(new LoginWithCustomIDRequest
+                {
+                    CustomId = SystemInfo.deviceUniqueIdentifier,
+                    CreateAccount = true
+                }
+                ,
+                result => { Debug.Log("ログイン成功"); },
+                error => { Debug.Log("ログイン失敗"); });
+        }
     }
 }

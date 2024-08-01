db51b95a78722351a3c7fdcd6291c24ebf7587b2
Hikanyan <hikanyan0131@gmail.com>
2024-08-01 15:45:04 +0900

    [Update]

--- Assets/ChronicleDimensionProject/Program/Scripts/UI/SampleStaticUIView.cs
+++ Assets/ChronicleDimensionProject/Program/Scripts/UI/SampleStaticUIView.cs
@@ -1,4 +1,8 @@
-﻿namespace ChronicleDimensionProject.UI
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ChronicleDimensionProject.UI
 {
     /// <summary>
     /// 静的なUIの表示・非表示を管理するクラス
@@ -7,5 +11,13 @@
     public class SampleStaticUIView : StaticUIView
     {
         // ここに処理を追加
+        [SerializeField] Button _showButton;
+        [SerializeField] Button _hideButton;
+
+        private void Start()
+        {
+            _showButton.onClick.AddListener(() => { Show(); });
+            _hideButton.onClick.AddListener(() => { Hide(); });
+        }
     }
 }

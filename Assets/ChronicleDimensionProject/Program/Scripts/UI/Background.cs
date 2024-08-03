036d9067b3421580400cc8641fa9bacc889211de
Hikanyan <hikanyan0131@gmail.com>
2024-08-03 23:00:27 +0900

    [Add]

---
+++ Assets/ChronicleDimensionProject/Program/Scripts/UI/Background.cs
@@ -0,0 +1,26 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace ChronicleDimensionProject.UI
+{
+    public class Background : MonoBehaviour, IUIView
+    {
+        private GameObject _background;
+
+        public void Start()
+        {
+            TryGetComponent(out _background);
+            
+        }
+
+        public UniTask Show()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public UniTask Hide()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

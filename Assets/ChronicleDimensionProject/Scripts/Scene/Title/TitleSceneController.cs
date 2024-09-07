3de82e8d947551957ad6366cd04ebf1859d34deb
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-09-07 18:29:20 +0900

    feat : タイトル

---
+++ Assets/ChronicleDimensionProject/Scripts/Scene/Title/TitleSceneController.cs
@@ -0,0 +1,52 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using VContainer;
+using VContainer.Unity;
+using SceneManager = ChronicleDimensionProject.System.SceneManager;
+
+namespace ChronicleDimensionProject.Title
+{
+    public class TitleSceneController : IStartable
+    {
+        private readonly SceneManager _sceneManager;
+
+        [Inject]
+        public TitleSceneController(SceneManager sceneManager)
+        {
+            _sceneManager = sceneManager;
+        }
+
+        public void Start()
+        {
+            // TitleSceneの起動時にManagerSceneをロードする
+            StartGameAsync().Forget();
+        }
+
+        private async UniTaskVoid StartGameAsync()
+        {
+            await _sceneManager.LoadSceneAsync("TitleScene", LoadSceneMode.Additive);
+        }
+    }
+
+    public class GameSceneController : IStartable
+    {
+        private readonly SceneManager _sceneManager;
+
+        [Inject]
+        public GameSceneController(SceneManager sceneManager)
+        {
+            _sceneManager = sceneManager;
+        }
+
+        public void Start()
+        {
+            // GameSceneの起動時にManagerSceneをロードする
+            StartGameAsync().Forget();
+        }
+
+        private async UniTaskVoid StartGameAsync()
+        {
+            await _sceneManager.LoadSceneAsync("GameScene", LoadSceneMode.Additive);
+        }
+    }
+}

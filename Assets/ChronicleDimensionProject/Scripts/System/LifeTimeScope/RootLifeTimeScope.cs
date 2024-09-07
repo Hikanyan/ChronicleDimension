9acef2f17e1583a7f1f02e2f0b0e86c77e2195a6
Hikanyan <hikanyan0131@gmail.com>
2024-09-07 17:05:11 +0900

    feat :VContainer

---
+++ Assets/ChronicleDimensionProject/Scripts/System/LifeTimeScope/RootLifeTimeScope.cs
@@ -0,0 +1,24 @@
+﻿using ChargeCatProject.System;
+using Photon.Pun.Demo.PunBasics;
+using VContainer;
+using VContainer.Unity;
+
+namespace ChronicleDimensionProject.System
+{
+    public class RootLifeTimeScope : LifetimeScope
+    {
+        // LifetimeScopeが開始されるときに呼び出されるメソッド
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<GameManager>(Lifetime.Singleton);
+            
+            
+            // FadeManagerの登録
+            builder.Register<IFadeStrategy, BasicFadeStrategy>(Lifetime.Singleton);
+            builder.Register<FadeManager>(Lifetime.Singleton);
+            builder.RegisterEntryPoint<FadeView>(); // MonoBehaviourの場合はEntryPointで登録
+            
+            
+        }
+    }
+}

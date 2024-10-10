473b32428c84889f86be14b2b5d53f73823d7e36
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-10-11 00:06:22 +0900

    Fix : Auto

--- Assets/ChronicleDimensionProject/Scripts/Scene/RhythmGameScene/Core/Notes/AutoMode.cs
+++ Assets/ChronicleDimensionProject/Scripts/Scene/RhythmGameScene/Core/Notes/AutoMode.cs
@@ -1,35 +1,25 @@
-﻿using UnityEngine;
+﻿using ChronicleDimensionProject.Common;
+using UnityEngine;
 
 namespace ChronicleDimensionProject.RhythmGame.Notes
 {
-    public class AutoMode:MonoBehaviour
+    public class AutoMode:Singleton<AutoMode>
     {
-        public static AutoMode Instance;                      //オートモードのBoolを引き継ぐためのシングルトン
-        [HideInInspector] public bool _autoMode = false;   //オートモード初期値false
+       [HideInInspector] public bool _autoMode = false;   //オートモード初期値false
 
-        private void Awake()                                //シングルトンのお約束
-        {
-            if (Instance == null)
-            {
-                DontDestroyOnLoad(this.gameObject);             //このゲームオブジェクトが壊され無くなった
-                Instance = this;
-            }
-            else
-            {
-                Destroy(this.gameObject);
-            }
-        }
+
         public void OnClickAutoModeButton()
         {
-            if (_autoMode == false)
+            switch (_autoMode)
             {
-                _autoMode = true;
-                Debug.Log($"オートモード{_autoMode}");
-            }
-            else if (_autoMode == true)
-            {
-                _autoMode = false;
-                Debug.Log($"オートモード{_autoMode}");
+                case false:
+                    _autoMode = true;
+                    Debug.Log($"オートモード{_autoMode}");
+                    break;
+                case true:
+                    _autoMode = false;
+                    Debug.Log($"オートモード{_autoMode}");
+                    break;
             }
         }
     }
